edbe38b8af1eb1d5eb462b548c64a39263cb4039
Mohamed Ramadan <60930725+MoRmdn@users.noreply.github.com>
2022-09-29 21:46:38 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:new_project/profile_page.dart';
 
@@ -9,19 +11,33 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
+
+  void measureNumber() {
+    double x = 42662;
+    double y = 0;
+
+    for (var i = 0; i < x; i++) {
+      y = x * 0.10;
+      x = x - y;
+      log("$x + $y  + $i");
+      if (x <= 1) {
+        return;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    measureNumber();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color(0xff0091BE),
-          secondary: Colors.white,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          brightness: Brightness.light,
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: const Color(0xff0091BE),
+            secondary: Colors.white,
+          ),
         ),
-      ),
-      home: const Profile()
-    );
+        home: const Profile());
   }
 }
-
